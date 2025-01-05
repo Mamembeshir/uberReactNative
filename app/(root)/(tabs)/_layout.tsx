96203b4417e5d0ad3bfa3bbe5954001e245a8501
mohammed <mamemohammed2017@gmail.com>
2025-01-04 19:52:23 -0800

    chnaged the tababr style

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -4,9 +4,9 @@ import { Tabs } from 'expo-router'
 import { icons } from '@/constants'
 const
     TabIcon = ({ focused, source }: { source: ImageSourcePropType, focused: boolean }) => (
-        <View>
-            <View >
-                <Image source={source} />
+        <View className={`flex flex-row justify-center items-center rounded-full ${focused ? `bg-general-300` : ''}`}>
+            <View className={`rounded-full items-center justify-center w-12 h-12 ${focused ? `bg-general-400` : ''}`}>
+                <Image source={source} tintColor="white" resizeMode='contain' className='h-7 w-7' />
             </View>
         </View>
     )
@@ -15,9 +15,42 @@ export default function _layout() {
         <Tabs
             initialRouteName='home'
             screenOptions={{
-                tabBarActiveTintColor: "white"
+                tabBarActiveTintColor: "white",
+                tabBarInactiveTintColor: "white",
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    backgroundColor: "#333333",
+                    borderRadius: 15,
+                    paddingBottom: 0,
+                    overflow: "hidden",
+                    marginHorizontal: 20,
+                    marginBottom: 20,
+                    height: 70,
+                    display: "flex",
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    flexDirection: "row",
+                    position: 'absolute'
+
+
+                }
             }}
         >
+            <Tabs.Screen name='home' options={{
+                title: "Home",
+                tabBarIcon: ({ focused }) => <TabIcon focused={focused} source={icons.home} />
+
+            }} />
+            <Tabs.Screen name='home' options={{
+                title: "Home",
+                tabBarIcon: ({ focused }) => <TabIcon focused={focused} source={icons.home} />
+
+            }} />
+            <Tabs.Screen name='home' options={{
+                title: "Home",
+                tabBarIcon: ({ focused }) => <TabIcon focused={focused} source={icons.home} />
+
+            }} />
             <Tabs.Screen name='home' options={{
                 title: "Home",
                 tabBarIcon: ({ focused }) => <TabIcon focused={focused} source={icons.home} />
